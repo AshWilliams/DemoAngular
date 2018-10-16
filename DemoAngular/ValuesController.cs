f3bf9d0795825572ac943759948f6bc54a03a66c
Robert Rozas Navarro <rorozasn@microsoft.com>
2018-10-16 17:43:25 -0500

    Se añade proyecto de prueba con última versión de angular

---
+++ DemoAngular/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoAngular
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            return await Task.Run(()=> new string[] { "Proyecto", "Angular", "+", ".Net Core", "Web", "API?" });
+        }
+
+    }
+}
